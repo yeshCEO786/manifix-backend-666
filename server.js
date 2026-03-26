d3bf0eb82ef1f966e16edd542f003263f39873f2
yeshCEO786 <rajanayeshaswiniceo78631888@gmail.com>
2026-03-26 18:15:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 // server.js
-import 'dotenv/config'; // this loads .env immediately
-import app from './src/app.js';
-import "./jobs/expirePremium.js";
+import 'dotenv/config';           // Load .env locally
+import app from './src/app.js';   // Your Express app
+import './jobs/expirePremium.js'; // Start scheduled jobs
+
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
