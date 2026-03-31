aa845666c0f078f0a47d540f214075b557f5dba0
yeshCEO786 <rajanayeshaswiniceo78631888@gmail.com>
2026-04-01 00:17:33 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -22,24 +22,22 @@ const app = express();
 /* ================= TRUST PROXY ================= */
 app.set("trust proxy", 1); // important for Railway/Vercel
 
-/* ================= SECURITY ================= */
+/* / SECURITY ================= */
 app.use(helmet());
 app.use(
   rateLimit({
     windowMs: 15 * 60 * 1000,
-    max: config.security.rateLimit || 100,
+    max: config.security.rateLimit || 5500,
     message: "⚠️ Too many requests from this IP, please try again later.",
   })
 );
 
-/* ================= CORS ================= */
-app.use(
-  cors({
-    origin: config.corsOrigin || "*",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true
+}));
 
 /* ================= BODY PARSER ================= */
 app.use(express.json({ limit: "10mb" }));
